2fdcd8dfae942d558e7ac13884ca798348de589e
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-03-21 18:09:11 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,13 +1,13 @@
 class Translation(object):
-    START_TEXT = """Hi
-, I'am a simple Link to telegram file or media uploader bot with permanent thumbnail support.
+    START_TEXT = """<b>Hi
+, I'am a simple Link to telegram file or media uploader bot with permanent thumbnail support.💯</b>
 
-/help for more...
+Click /help For More.....
 
-Support Channel : @VKProjects"""
+❤Mode By @VKProjects"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "<b>👉 Create own Clone Bot.. </b>  /help for Details"
+    UPGRADE_TEXT = "Contact @VkBotssupportBot"
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
